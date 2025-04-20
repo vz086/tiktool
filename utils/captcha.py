640b14f0e9f358bc08443a5b87b6037b82adebd7
Adam B <anniebwby@gmail.com>
2025-04-20 17:08:06 +0200

    Add files via upload

--- utils/captcha.py
+++ utils/captcha.py
@@ -7,7 +7,7 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 
 class CaptchaWindow(customtkinter.CTkToplevel):
-    def __init__(self):
+    def __init__(self, is_retry=False):
         super().__init__()
         self.geometry("400x500")
         self.title("Captcha Entry")
@@ -21,6 +21,11 @@ class CaptchaWindow(customtkinter.CTkToplevel):
         img = customtkinter.CTkImage(Image.open("captcha.png"), size=(300, 100))
         self.image_label = customtkinter.CTkLabel(self, image=img, text="")
         self.image_label.pack(pady=20)
+        
+        # Show error message if retry
+        if is_retry:
+            self.error_label = customtkinter.CTkLabel(self, text="Incorrect captcha! Please try again.", text_color="red")
+            self.error_label.pack(pady=10)
         
         # Create entry for captcha input
         self.captcha_entry = customtkinter.CTkEntry(self, width=200, placeholder_text="Enter Captcha")
